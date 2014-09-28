997e682a8e65525925c37d16d4e234cf538d950b
Yunming Zhang <yunmingzhang@30-71-153.dynamic.csail.mit.edu>
2014-09-28 14:18:49 -0400

    adding latex directory for writing the report

--- homework1/evaluateMandLambda.m
+++ homework1/evaluateMandLambda.m
@@ -12,26 +12,28 @@ function evaluateMandLambda(computeWeightFunction )
     end
     
     
-    M = 6;
-    errA = zeros(length(lambdarange), 1);
-    errB = zeros(length(lambdarange), 1);
-    i = 0;
-    for lambda = lambdarange
-        i = i+ 1;
-        lambda
-        weightA = computeWeightFunction(XA', YA', lambda, M);
-        weightB = computeWeightFunction(XB', YB', lambda, M);
-        sseVA = computeSSE3(weightA, XV', YV');
-        sseVB = computeSSE3(weightB, XV', YV');
-        errA(i) = sseVA;
-        errB(i) = sseVB;
-    end
+    for M = 1:2
+        errA = zeros(length(lambdarange), 1);
+        errB = zeros(length(lambdarange), 1);
+        i = 0;
+        for lambda = lambdarange
+            i = i+ 1;
+            
+            weightA = computeWeightFunction(XA', YA', lambda, M);
+            weightB = computeWeightFunction(XB', YB', lambda, M);
+            sseVA = computeSSE3(weightA, XV', YV');
+            sseVB = computeSSE3(weightB, XV', YV');
+            errA(i) = sseVA;
+            errB(i) = sseVB;
+        end
+        figure();
+        plot(log(lambdarange'), errA, 'og', 'MarkerSize', 10);
+        title('Graph of Sine and Cosine Between -2\pi and 2\pi')
         
+        figure();
+        plot(log(lambdarange'), errB, 'ob', 'MarkerSize', 10);
+    end
     
-    figure();
-    plot(log(lambdarange'), errA, 'og', 'MarkerSize', 10);
-    
-    figure();
-    plot(log(lambdarange'), errB, 'ob', 'MarkerSize', 10);
+
  
 end
