1cbf07c2ff43debcc78cc2200503c22e52dfbf98
Yunming Zhang <yunmingzhang@dhcp-18-111-38-214.dyn.mit.edu>
2014-09-30 01:08:12 -0400

    putting in all the figures for problem2

--- homework1/problem2.m
+++ homework1/problem2.m
@@ -1,29 +1,30 @@
 function problem2
 
 clear all
+close all
 clc
 
 %y = sin(2 pi x) + N(0,0.3),
-data = importdata('curvefitting.txt');
-
-X = data(1,:);
-Y = data(2,:);
-
-X = X';
-Y = Y';
-
-figure;
-
-plot(X, Y, 'o', 'MarkerSize', 10);
-xlabel('x');
-ylabel('y');
-
-hold on
-
-%plot the sin2pi function
-fplot(@(x) sin(2*pi*x), [0,1, -2, 2], 'r') 
-
-hold on
+% data = importdata('curvefitting.txt');
+% 
+% X = data(1,:);
+% Y = data(2,:);
+% 
+% X = X';
+% Y = Y';
+% 
+% figure;
+% 
+% plot(X, Y, 'o', 'MarkerSize', 10);
+% xlabel('x');
+% ylabel('y');
+% 
+% hold on
+% 
+% %plot the sin2pi function
+% fplot(@(x) sin(2*pi*x), [0,1, -2, 2], 'r') 
+% 
+% hold on
  %plotMLEforM(X, Y, 1);
  
  %plotMLEforM(X, Y, 3);
@@ -51,7 +52,8 @@ hold on
 % plot(xrange, yrange, 'g');
 
 %compareSSEGradient();
-testGradientDescent();
+part1();
+%testGradientDescent();
 
 
 end
@@ -59,9 +61,44 @@ end
 
 function testGradientDescent ()
 
-%minTheta = gradientDescentML([1; 1], @computeSSE, @computeSSEGradient, 0.05, 0.000001)
-%minTheta2 = gradientDescentML2([1; 1], @computeSSE2, @computeSSEGradient2, 0.05, 0.000001, 1)
 
+data = importdata('curvefitting.txt');
+
+X = data(1,:);
+Y = data(2,:);
+
+X = X';
+Y = Y';
+
+figure;
+
+plot(X, Y, 'o', 'MarkerSize', 10);
+xlabel('x');
+ylabel('y');
+
+hold on
+
+%plot the sin2pi function
+fplot(@(x) sin(2*pi*x), [0,1, -2, 2], 'r') 
+
+hold on
+minTheta = gradientDescentML([1; 1], @computeSSE, @computeSSEGradient, 0.05, 0.000001)
+plotWithTheta(minTheta);
+
+
+figure();
+plot(X, Y, 'o', 'MarkerSize', 10);
+xlabel('x');
+ylabel('y');
+
+hold on
+
+%plot the sin2pi function
+fplot(@(x) sin(2*pi*x), [0,1, -2, 2], 'r') 
+
+hold on
+minTheta0 = gradientDescentML([1], @computeSSE2, @computeSSEGradient2, 0.05, 0.000001)
+plotWithTheta(minTheta0);
 
 %M = 2 not including the ones column
 % minTheta3 = gradientDescentML([0; 0; 0], @computeSSE2, @computeSSEGradient2, 0.05, 0.0000001)
@@ -71,10 +108,21 @@ function testGradientDescent ()
 
 %M = 3
 %easy to get into local minimum
-minTheta4 = gradientDescentML(zeros(4,1), @computeSSE2, @computeSSEGradient2, 0.05, 0.0000001)
-% plotWithTheta(minTheta4);
+figure();
+plot(X, Y, 'o', 'MarkerSize', 10);
+xlabel('x');
+ylabel('y');
 
 hold on
+
+%plot the sin2pi function
+fplot(@(x) sin(2*pi*x), [0,1, -2, 2], 'r') 
+
+hold on
+minTheta4 = gradientDescentML(zeros(4,1), @computeSSE2, @computeSSEGradient2, 0.05, 0.0000001)
+plotWithTheta(minTheta4);
+
+
 % M = 9
 %as the dimensions increases, the step size needs to be smaller
 
@@ -84,12 +132,105 @@ initGuess = [0.35; 100; -5321.83; 48568.31; -231639.30; 640042.26; -1061800.52; 
 %initGuess = [0.35; 0; -5321.83; 48568.31; 0; 640042.26; -1061800.52; 1042400.18; -557682.99; 125201.43;];
 
 %0.05 for step size is alwyas too big
+figure();
+plot(X, Y, 'o', 'MarkerSize', 10);
+xlabel('x');
+ylabel('y');
 
+hold on
+
+%plot the sin2pi function
+fplot(@(x) sin(2*pi*x), [0,1, -2, 2], 'r') 
+
+hold on
 minTheta10 = gradientDescentML(initGuess, @computeSSE2, @computeSSEGradient2, 0.03, 0.0000001)
 plotWithTheta(minTheta10);
 
 end
 
+
+function part1()
+data = importdata('curvefitting.txt');
+
+X = data(1,:);
+Y = data(2,:);
+
+X = X';
+Y = Y';
+
+figure;
+
+plot(X, Y, 'o', 'MarkerSize', 10);
+xlabel('x');
+ylabel('y');
+
+hold on
+
+%plot the sin2pi function
+fplot(@(x) sin(2*pi*x), [0,1, -2, 2], 'r') 
+
+hold on
+plotMLEforM(X, Y, 0);
+
+
+figure();
+plot(X, Y, 'o', 'MarkerSize', 10);
+xlabel('x');
+ylabel('y');
+
+hold on
+
+%plot the sin2pi function
+fplot(@(x) sin(2*pi*x), [0,1, -2, 2], 'r') 
+
+hold on
+plotMLEforM(X, Y, 1);
+
+%M = 2 not including the ones column
+% minTheta3 = gradientDescentML([0; 0; 0], @computeSSE2, @computeSSEGradient2, 0.05, 0.0000001)
+% plotWithTheta(minTheta3);
+
+% hold on
+
+%M = 3
+%easy to get into local minimum
+figure();
+plot(X, Y, 'o', 'MarkerSize', 10);
+xlabel('x');
+ylabel('y');
+
+hold on
+
+%plot the sin2pi function
+fplot(@(x) sin(2*pi*x), [0,1, -2, 2], 'r') 
+
+hold on
+plotMLEforM(X, Y, 3);
+
+
+% M = 9
+%as the dimensions increases, the step size needs to be smaller
+
+%the answer [0.35; 232.37; -5321.83; 48568.31; -231639.30; 640042.26; -1061800.52; 1042400.18; -557682.99; 125201.43;];
+%this works 
+initGuess = [0.35; 100; -5321.83; 48568.31; -231639.30; 640042.26; -1061800.52; 1042400.18; -557682.99; 125201.43;];
+%initGuess = [0.35; 0; -5321.83; 48568.31; 0; 640042.26; -1061800.52; 1042400.18; -557682.99; 125201.43;];
+
+%0.05 for step size is alwyas too big
+figure();
+plot(X, Y, 'o', 'MarkerSize', 10);
+xlabel('x');
+ylabel('y');
+
+hold on
+
+%plot the sin2pi function
+fplot(@(x) sin(2*pi*x), [0,1, -2, 2], 'r') 
+
+hold on
+plotMLEforM(X, Y, 9);
+
+end
 
 function compareSSEGradient
     x = [[10, 10, 12, -20]; [12, 2, 20, 15];];
